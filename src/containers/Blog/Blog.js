1a99ef7b45e87ce02bf8babb616eb1d5ecd6936f
alekhranjan75 <alekhranjan75@gmail.com>
2020-04-16 17:04:09 +0530

    Fetching data and displaying using axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,39 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    _isMounted = false;
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        this._isMounted = true;
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            
+            if (this._isMounted) {
+                this.setState({
+                    posts: response.data
+                })
+            }
+            console.log(response)
+        })
+    }
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title = {post.title}  key = {post.id}/>
+        });
+        
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
